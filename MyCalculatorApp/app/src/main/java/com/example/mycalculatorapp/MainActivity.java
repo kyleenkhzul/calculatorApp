e8c79961cd601bf908b019812d1e564463bd3e46
kyleenkhzul <90794184+kyleenkhzul@users.noreply.github.com>
2022-08-24 11:02:03 -0500

    created second activity java file
    - binded intent with the GPA button
    - created xml layout for the GPA screen
    - coded the GPA to calculate
    
    broken:
    - normal button
    - calculate button crashes
    - if the user types nothing it crashes

--- MyCalculatorApp/app/src/main/java/com/example/mycalculatorapp/MainActivity.java
+++ MyCalculatorApp/app/src/main/java/com/example/mycalculatorapp/MainActivity.java
@@ -2,19 +2,18 @@ package com.example.mycalculatorapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
         }
 
     public void findSum(View view) {
@@ -65,8 +64,32 @@ public class MainActivity extends AppCompatActivity {
         numberSumTV.setText("" + quotient);
     }
 
-    public void switchScreens(View v) {
-        EditText editText = findViewById(R.id.button2);
+    public void findGPA(View view) {
+        EditText numberFirstClass = findViewById(R.id.firstClass);
+        EditText numberSecondClass = findViewById(R.id.secondClass);
+        EditText numberThirdClass = findViewById(R.id.thirdClass);
+        EditText numberFourthClass = findViewById(R.id.fourthClass);
+        EditText numberFifthClass = findViewById(R.id.fifthClass);
+        EditText numberSixthClass = findViewById(R.id.sixthClass);
+        EditText numberSeventhClass = findViewById(R.id.seventhClass);
+        TextView numberSumTV = findViewById(R.id.resultGPA);
+
+        double num1 = Integer.parseInt((numberFirstClass.getText().toString()));
+        double num2 = Integer.parseInt(numberSecondClass.getText().toString());
+        double num3 = Integer.parseInt((numberThirdClass.getText().toString()));
+        double num4 = Integer.parseInt(numberFourthClass.getText().toString());
+        double num5 = Integer.parseInt((numberFifthClass.getText().toString()));
+        double num6 = Integer.parseInt(numberSixthClass.getText().toString());
+        double num7 = Integer.parseInt(numberSeventhClass.getText().toString());
+
+        double sum = num1 + num2 + num3 + num4 + num5 + num6 + num7;
+        double temp = sum / 7;
+        double quotient = (double)(Math.round(temp * 100.0) / 100.00);
+        numberSumTV.setText("" + quotient);
     }
 
+    public void switchScreens(View view) {
+        Intent intent = new Intent(this, SecondActivity.class);
+        startActivity(intent);
+    }
 }
