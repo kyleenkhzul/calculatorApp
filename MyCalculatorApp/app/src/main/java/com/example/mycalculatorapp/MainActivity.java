83d1b36f0e50d1203d20cb44a5fcadb8b53fd0d9
kyleenkhzul <90794184+kyleenkhzul@users.noreply.github.com>
2022-08-25 11:04:14 -0500

    fixed the calculations for the gpa calculator
    problems:
    - store number as astring variable
    - either do try-catch
    - check if its null only if its a string variable
    - left off on user input and toast message implementation

--- MyCalculatorApp/app/src/main/java/com/example/mycalculatorapp/MainActivity.java
+++ MyCalculatorApp/app/src/main/java/com/example/mycalculatorapp/MainActivity.java
@@ -64,30 +64,6 @@ public class MainActivity extends AppCompatActivity {
         numberSumTV.setText("" + quotient);
     }
 
-    public void findGPA(View view) {
-        EditText numberFirstClass = findViewById(R.id.firstClass);
-        EditText numberSecondClass = findViewById(R.id.secondClass);
-        EditText numberThirdClass = findViewById(R.id.thirdClass);
-        EditText numberFourthClass = findViewById(R.id.fourthClass);
-        EditText numberFifthClass = findViewById(R.id.fifthClass);
-        EditText numberSixthClass = findViewById(R.id.sixthClass);
-        EditText numberSeventhClass = findViewById(R.id.seventhClass);
-        TextView numberSumTV = findViewById(R.id.resultGPA);
-
-        double num1 = Integer.parseInt((numberFirstClass.getText().toString()));
-        double num2 = Integer.parseInt(numberSecondClass.getText().toString());
-        double num3 = Integer.parseInt((numberThirdClass.getText().toString()));
-        double num4 = Integer.parseInt(numberFourthClass.getText().toString());
-        double num5 = Integer.parseInt((numberFifthClass.getText().toString()));
-        double num6 = Integer.parseInt(numberSixthClass.getText().toString());
-        double num7 = Integer.parseInt(numberSeventhClass.getText().toString());
-
-        double sum = num1 + num2 + num3 + num4 + num5 + num6 + num7;
-        double temp = sum / 7;
-        double quotient = (double)(Math.round(temp * 100.0) / 100.00);
-        numberSumTV.setText("" + quotient);
-    }
-
     public void switchScreens(View view) {
         Intent intent = new Intent(this, SecondActivity.class);
         startActivity(intent);
--- MyCalculatorApp/app/src/main/java/com/example/mycalculatorapp/SecondActivity.java
+++ MyCalculatorApp/app/src/main/java/com/example/mycalculatorapp/SecondActivity.java
@@ -2,15 +2,59 @@ package com.example.mycalculatorapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class SecondActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.gpacalculator);
         Intent intent = getIntent();
+    }
+    public void findGPA(View view) {
+        EditText numberFirstClass = findViewById(R.id.firstClass);
+        EditText numberSecondClass = findViewById(R.id.secondClass);
+        EditText numberThirdClass = findViewById(R.id.thirdClass);
+        EditText numberFourthClass = findViewById(R.id.fourthClass);
+        EditText numberFifthClass = findViewById(R.id.fifthClass);
+        EditText numberSixthClass = findViewById(R.id.sixthClass);
+        EditText numberSeventhClass = findViewById(R.id.seventhClass);
+        TextView numberSumTV = findViewById(R.id.resultGPA);
+
+        Context context = getApplicationContext();
+        CharSequence text = "Please enter a GPA in the boxes";
+        int duration = Toast.LENGTH_SHORT;
+        Toast toast = Toast.makeText(context, text, duration);
+
+
+         int num1 = Integer.parseInt((numberFirstClass.getText().toString()));
+         int num2 = Integer.parseInt(numberSecondClass.getText().toString());
+         int num3 = Integer.parseInt((numberThirdClass.getText().toString()));
+         int num4 = Integer.parseInt(numberFourthClass.getText().toString());
+         int num5 = Integer.parseInt((numberFifthClass.getText().toString()));
+         int num6 = Integer.parseInt(numberSixthClass.getText().toString());
+         int num7 = Integer.parseInt(numberSeventhClass.getText().toString());
+
+
+        if(numberFirstClass.getText().toString() == "" ||
+                numberSecondClass.getText().toString() == "" ||
+                numberThirdClass.getText().toString() == "" ||
+                numberFourthClass.getText().toString() == "" ||
+                numberFifthClass.getText().toString() == "" ||
+                numberSixthClass.getText().toString() == "" ||
+                numberSeventhClass.getText().toString() == "") {
+            toast.show();
+        }
+
+         double sum = (num1 + num2 + num3 + num4 + num5 + num6 + num7);
+         double temp = sum / 7;
+         double quotient = (Math.round(temp * 100.0) / 100.00);
+        numberSumTV.setText("" + quotient);
     }
 }
